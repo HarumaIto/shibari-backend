a27a3c98034a41c09a485afd6da06b727fbe5af2
HarumaIto <itoharm@gmail.com>
2026-03-01 22:24:29 +0900

    remove: space

--- functions/src/common/getStartOfWeek.ts
+++ functions/src/common/getStartOfWeek.ts
@@ -7,7 +7,7 @@ export const getStartOfWeek = (): Date => {
   const dayOfWeek = startOfWeek.getDay();
 
   const diffToMonday = startOfWeek.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1);
-  
+
   startOfWeek.setDate(diffToMonday);
   startOfWeek.setHours(0, 0, 0, 0);
   return startOfWeek;
