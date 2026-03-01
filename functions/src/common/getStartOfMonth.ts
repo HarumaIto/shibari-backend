4e934b405f97bbc5ac6d57da4c583f246b2d5ab2
HarumaIto <itoharm@gmail.com>
2026-03-01 22:20:24 +0900

    fix: typo

--- functions/src/common/getStartOfMonth.ts
+++ functions/src/common/getStartOfMonth.ts
@@ -3,7 +3,7 @@ import { getJstNow } from './getJstNow';
 export const getStartOfMonth = (): Date => {
   const jstNow = getJstNow();
 
-  const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+  const startOfMonth = new Date(jstNow.getFullYear(), jstNow.getMonth(), 1);
   startOfMonth.setHours(0, 0, 0, 0);
   return startOfMonth;
 };
